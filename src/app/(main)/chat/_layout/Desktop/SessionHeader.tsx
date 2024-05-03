576edacd1b1135e167cc8ab624aeb093bc31c70c
GOvEy1nw <45126527+GOvEy1nw@users.noreply.github.com>
2024-05-03 17:33:14 +0100

    Update SessionHeader.tsx

--- src/app/(main)/chat/_layout/Desktop/SessionHeader.tsx
+++ src/app/(main)/chat/_layout/Desktop/SessionHeader.tsx
@@ -35,7 +35,7 @@ const Header = memo(() => {
     <Flexbox className={styles.top} gap={16} padding={16}>
       <Flexbox distribution={'space-between'} horizontal>
         <Flexbox align={'center'} gap={4} horizontal>
-          <Logo className={styles.logo} size={100} type={'text'} style={{ paddingBottom: '15px' }}  />
+          <Logo className={styles.logo} size={100} style={{ paddingBottom: '15px' }} type={'text'} />
           {enableWebrtc && <SyncStatusTag />}
         </Flexbox>
         {showCreateSession && (

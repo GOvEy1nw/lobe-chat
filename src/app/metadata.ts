e950bb4087435d81fcb6f546a61f8b9f0607f79e
GOvEy1nw <45126527+GOvEy1nw@users.noreply.github.com>
2024-05-08 10:13:42 +0100

    Rais

--- src/app/metadata.ts
+++ src/app/metadata.ts
@@ -6,7 +6,7 @@ import { OFFICIAL_URL } from '@/const/url';
 
 import pkg from '../../package.json';
 
-const title = 'LobeChat';
+const title = 'Rais';
 const { description, homepage } = pkg;
 
 const { SITE_URL = OFFICIAL_URL } = getServerConfig();
@@ -24,7 +24,7 @@ const metadata: Metadata = {
   icons: {
     apple: '/icons/apple-touch-icon.png',
     icon: '/favicon.ico',
-    shortcut: '/favicon-32x32.ico',
+    shortcut: '/favicon.ico',
   },
   manifest: noManifest ? undefined : '/manifest.json',
   metadataBase: new URL(SITE_URL),
@@ -53,7 +53,7 @@ const metadata: Metadata = {
 
   title: {
     default: title,
-    template: '%s · LobeChat',
+    template: '%s · Rais',
   },
   twitter: {
     card: 'summary_large_image',
